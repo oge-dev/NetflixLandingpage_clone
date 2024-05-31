b8e471a9a9773276fe8fed11ed7478b33eb20e1f
julietoge <julietogechi27@gmail.com>
2024-05-31 02:43:38 +0100

    update main.js

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// JavaScript to toggle the content visibility and change icons
+var coll = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    // Close all other collapsibles
+    for (let j = 0; j < coll.length; j++) {
+      if (coll[j] !== this) {
+        coll[j].classList.remove("active");
+        coll[j].nextElementSibling.style.display = "none";
+        coll[j].querySelector('.icon').classList.remove('minus');
+        coll[j].querySelector('.icon').classList.add('plus');
+      }
+    }
+    // Toggle the clicked collapsible
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    var icon = this.querySelector('.icon');
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      icon.classList.remove('minus');
+      icon.classList.add('plus');
+    } else {
+      content.style.display = "block";
+      icon.classList.remove('plus');
+      icon.classList.add('minus');
+    }
+  });
+}
