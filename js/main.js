aae94236be801802a2b67e1b4296f9e6bea7affe
julietoge <julietogechi27@gmail.com>
2024-06-01 19:25:53 +0100

    update collapsible at index.html

--- js/main.js
+++ js/main.js
@@ -2,28 +2,28 @@
 var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     // Close all other collapsibles
     for (let j = 0; j < coll.length; j++) {
       if (coll[j] !== this) {
         coll[j].classList.remove("active");
         coll[j].nextElementSibling.style.display = "none";
-        coll[j].querySelector('.icon').classList.remove('minus');
-        coll[j].querySelector('.icon').classList.add('plus');
+        coll[j].querySelector(".icon").classList.remove("minus");
+        coll[j].querySelector(".icon").classList.add("plus");
       }
     }
     // Toggle the clicked collapsible
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    var icon = this.querySelector('.icon');
+    var icon = this.querySelector(".icon");
     if (content.style.display === "block") {
       content.style.display = "none";
-      icon.classList.remove('minus');
-      icon.classList.add('plus');
+      icon.classList.remove("minus");
+      icon.classList.add("plus");
     } else {
       content.style.display = "block";
-      icon.classList.remove('plus');
-      icon.classList.add('minus');
+      icon.classList.remove("plus");
+      icon.classList.add("minus");
     }
   });
 }
